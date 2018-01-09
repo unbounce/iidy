74182ee73e5358dabbddaef504a1411ae8cf85fb
pauloancheta <paulo.ancheta@gmail.com>
2018-01-09 14:12:29 +1100

    use url to parse approvedtemplatelocation

--- src/requestApproveTemplate.ts
+++ src/requestApproveTemplate.ts
@@ -3,6 +3,7 @@ import { Md5 } from 'ts-md5/dist/md5';
 import * as fs from 'fs';
 import * as cli from 'cli-color';
 import * as path from 'path';
+import * as url from 'url';
 
 import { Arguments } from 'yargs';
 import { loadStackArgs, parseS3HttpUrl } from './cfn/index';
@@ -16,17 +17,21 @@ export async function requestApproveTemplate(argv: Arguments): Promise<number> {
   await configureAWS(stackArgs.Profile, stackArgs.Region);
 
   const cfnTemplate = await fs.readFileSync(stackArgs.Template);
-  const s3Url = parseS3HttpUrl(stackArgs.ApprovedTemplateLocation);
+  const approvedTemplateLocation = stackArgs.ApprovedTemplateLocation ? stackArgs.ApprovedTemplateLocation : "";
 
-  const hashedKey = path.join(
-    s3Url.key,
-    new Md5().appendStr(cfnTemplate.toString()).end().toString()
-  );
+  const s3Url = url.parse(approvedTemplateLocation);
+  const s3Path = s3Url.path ? s3Url.path : "";
+  const s3Bucket = s3Url.hostname ? s3Url.hostname : "";
+
+  const fileName = new Md5().appendStr(cfnTemplate.toString()).end().toString()
+  const fullFileName = `${fileName}${path.extname(stackArgs.Template)}`
+
+  const hashedKey = path.join(s3Path, fullFileName);
 
   try {
     await s3.headObject({
-      Bucket: s3Url.bucket,
-      Key: `${hashedKey}.yaml`
+      Bucket: s3Bucket,
+      Key: fullFileName
     }).promise();
 
     logSuccess(`👍 Your template has already been approved`);
@@ -34,12 +39,12 @@ export async function requestApproveTemplate(argv: Arguments): Promise<number> {
     if (e.code === "NotFound") {
       await s3.putObject({
         Body: cfnTemplate,
-        Bucket: s3Url.bucket,
-        Key: `${hashedKey}${path.extname(stackArgs.Template)}.pending`
+        Bucket: s3Bucket,
+        Key: `${fullFileName}.pending`
       }).promise();
 
-      logSuccess(`Successfully uploaded the cloudformation template to ${stackArgs.ApprovedTemplateLocation}`);
-      logSuccess(`Approve template with \`iidy approve ${hashedKey}.yaml\``);
+      logSuccess(`Successfully uploaded the cloudformation template to ${approvedTemplateLocation}`);
+      logSuccess(`Approve template with \`iidy approve ${fullFileName}\``);
     } else {
       throw new Error(e);
     }
