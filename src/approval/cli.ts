b3f275ef9d39926a1bd5d510bf61a0c80ce0645d
Tavis Rudd <tavis@damnsimple.com>
2018-05-15 15:47:28 -0700

    code formatting (tsfmt)

--- src/approval/cli.ts
+++ src/approval/cli.ts
@@ -1,46 +1,45 @@
 import * as yargs from 'yargs';
 
-import { description, Handler, wrapCommandHandler } from '../cli';
+import {description, Handler, wrapCommandHandler} from '../cli';
 
 export interface ApprovalCommands {
-    request: Handler;
-    review: Handler;
+  request: Handler;
+  review: Handler;
 }
 
 const lazyLoad = (fnname: keyof ApprovalCommands): Handler =>
-    (args) => require('../approval')[fnname](args);
+  (args) => require('../approval')[fnname](args);
 
 const lazy: ApprovalCommands = {
-    request: lazyLoad('request'),
-    review: lazyLoad('review')
+  request: lazyLoad('request'),
+  review: lazyLoad('review')
 }
 
 export function buildApprovalCommands(args: yargs.Argv, commands = lazy): yargs.Argv {
-    return args
-        .strict()
-        .demandCommand(1, 0)
-        .command(
-        'request <argsfile>',
-        description('request template approval'),
-        (args) => args
-            .demandCommand(0, 0)
-            .usage('Usage: iidy template-approval request stack-args.yaml')
-            .strict(),
-        wrapCommandHandler(commands.request)
-        )
+  return args
+    .strict()
+    .demandCommand(1, 0)
+    .command(
+    'request <argsfile>',
+    description('request template approval'),
+    (args) => args
+      .demandCommand(0, 0)
+      .usage('Usage: iidy template-approval request stack-args.yaml')
+      .strict(),
+    wrapCommandHandler(commands.request))
 
-        .command(
-        'review <url>',
-        description('review pending template approval request'),
-        (args) => args
-            .demandCommand(0, 0)
-            .option('context', {
-              type: 'number', default: 100,
-              description: description('how many lines of diff context to show')
-            })
-            .usage('Usage: iidy template-approval review s3://bucket/path.pending')
-            .strict(),
-        wrapCommandHandler(commands.review)
-        );
+    .command(
+    'review <url>',
+    description('review pending template approval request'),
+    (args) => args
+      .demandCommand(0, 0)
+      .option('context', {
+        type: 'number', default: 100,
+        description: description('how many lines of diff context to show')
+      })
+      .usage('Usage: iidy template-approval review s3://bucket/path.pending')
+      .strict(),
+    wrapCommandHandler(commands.review)
+    );
 
 }
