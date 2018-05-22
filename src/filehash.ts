6ead9e84cc9b276d729484b088b0fb31104e5cd3
Tavis Rudd <tavis@damnsimple.com>
2018-05-22 13:38:18 -0700

    factor `filehash` out into separate module.
    Prepatory refactoring for the rest of this branch.

---
+++ src/filehash.ts
@@ -0,0 +1,21 @@
+import * as fs from 'fs';
+import * as pathmod from 'path';
+import * as child_process from 'child_process';
+
+import normalizePath from './normalizePath';
+
+/** Calculate a sha256 hash of a file or directory. */
+export default (path0: string) => {
+  const path = normalizePath(path0);
+  // this assumes local files/dirs TODO validate that
+  if (!fs.existsSync(path)) {
+    throw new Error(`Invalid path ${path} for filehash`);
+  }
+  const isDir = fs.lstatSync(path).isDirectory();
+  const shasumCommand = 'shasum -p -a 256';
+  const hashCommand = isDir
+    ? `find ${path} -type f -print0 | xargs -0 ${shasumCommand} | ${shasumCommand}`
+    : `${shasumCommand} ${path}`;
+  const result = child_process.spawnSync(hashCommand, [], {shell: true});
+  return result.stdout.toString().trim().split(' ')[0];
+}
