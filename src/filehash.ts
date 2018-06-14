92fe57866048a750c617cc3d76f83ddb2b1fad8a
Tavis Rudd <tavis@damnsimple.com>
2018-06-14 16:49:35 -0700

    add support for base64 encoded filehashes in addition to hex

--- src/filehash.ts
+++ src/filehash.ts
@@ -5,7 +5,7 @@ import * as child_process from 'child_process';
 import normalizePath from './normalizePath';
 
 /** Calculate a sha256 hash of a file or directory. */
-export default (path0: string) => {
+export default (path0: string, format: 'hex'|'base64' = 'hex') => {
   const path = normalizePath(path0);
   // this assumes local files/dirs TODO validate that
   if (!fs.existsSync(path)) {
@@ -16,6 +16,13 @@ export default (path0: string) => {
   const hashCommand = isDir
     ? `find ${path} -type f -print0 | xargs -0 ${shasumCommand} | ${shasumCommand}`
     : `${shasumCommand} ${path}`;
+
   const result = child_process.spawnSync(hashCommand, [], {shell: true});
-  return result.stdout.toString().trim().split(' ')[0];
+  const hexHash = result.stdout.toString().trim().split(' ')[0];
+  switch (format) {
+    case 'hex':
+      return hexHash;
+    case 'base64':
+      return Buffer.from(hexHash, 'hex').toString('base64');
+  }
 }
