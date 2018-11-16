45cf42af578dabd67db9a6abe18ea0a278c5f52d
James Brennan <james@jamesbrennan.ca>
2018-11-16 22:29:22 +0000

    missingHelper doesn't support dotted.lookups
    https://github.com/wycats/handlebars.js/issues/1435

--- src/tests/test-visitor.ts
+++ src/tests/test-visitor.ts
@@ -6,7 +6,7 @@ import {VariablesVisitor} from '../preprocess/visitor';
 const mkTestEnv = ($envValues: pre.$EnvValues, GlobalAccumulator = {}) => ({
   GlobalAccumulator,
   $envValues,
-  Stack: []
+  Stack: [{ location: '0', path: '0' }]
 })
 
 describe('VariablesVisitor', () => {
@@ -18,11 +18,27 @@ describe('VariablesVisitor', () => {
     expect(visitor.variables).to.deep.equal(['bar'])
   });
 
+  it('extracts nested $! variables', function() {
+    const visitor = new VariablesVisitor();
+    visitor.visitNode({
+      a: new yaml.$include('foo.bar')
+    }, 'test', mkTestEnv({foo: {bar: 'baz'}}));
+    expect(visitor.variables).to.deep.equal(['foo.bar'])
+  });
+
   it('extracts handlebars variables', function() {
     const visitor = new VariablesVisitor();
     visitor.visitNode({
       foo: '{{ bar }}'
     }, 'test', mkTestEnv({bar: 'baz'}));
     expect(visitor.variables).to.deep.equal(['bar'])
+  });
+
+  it('extracts nested handlebars variables', function() {
+    const visitor = new VariablesVisitor();
+    visitor.visitNode({
+      foo: '{{ foo.bar }}'
+    }, 'test', mkTestEnv({foo: {bar: 'baz'} }));
+    expect(visitor.variables).to.deep.equal(['foo.bar'])
   });
 });
