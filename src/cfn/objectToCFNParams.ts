390428741f175521e784d25955a5b65b04af2e73
Tavis Rudd <tavis@damnsimple.com>
2019-12-02 15:16:35 -0800

    rm outdated TODO comment

--- src/cfn/objectToCFNParams.ts
+++ src/cfn/objectToCFNParams.ts
@@ -3,7 +3,6 @@ import * as _ from 'lodash';
 
 export default (obj: {[key: string]: string}): aws.CloudFormation.Parameters =>
   _.map(_.toPairs(obj),
-    // TODO handle UsePreviousValue for updates
     ([ParameterKey, ParameterValue]) => {
       return {ParameterKey, ParameterValue: ParameterValue.toString()}
     })
