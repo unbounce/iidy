c26c732783e44b4529870d046eb7d7051a9ecc9f
Tavis Rudd <tavis@damnsimple.com>
2019-12-09 16:18:51 -0800

    fix #178: filehash import shasum portability issue

--- src/filehash.ts
+++ src/filehash.ts
@@ -1,23 +1,30 @@
 import * as fs from 'fs';
-import * as child_process from 'child_process';
+import * as glob from 'glob';
+import {createHash} from 'crypto';
 
 import normalizePath from './normalizePath';
 
+const sha256sum = (filename: string) =>
+  createHash('sha256').update(fs.readFileSync(filename)).digest('hex');
+
 /** Calculate a sha256 hash of a file or directory. */
-export default (path0: string, format: 'hex'|'base64' = 'hex') => {
+export default (path0: string, format: 'hex' | 'base64' = 'hex') => {
   const path = normalizePath(path0);
   // this assumes local files/dirs TODO validate that
   if (!fs.existsSync(path)) {
     throw new Error(`Invalid path ${path} for filehash`);
   }
   const isDir = fs.lstatSync(path).isDirectory();
-  const shasumCommand = 'shasum -p -a 256';
-  const hashCommand = isDir
-    ? `find ${path} -type f -print0 | xargs -0 ${shasumCommand} | ${shasumCommand}`
-    : `${shasumCommand} ${path}`;
 
-  const result = child_process.spawnSync(hashCommand, [], {shell: true});
-  const hexHash = result.stdout.toString().trim().split(' ')[0];
+  let hexHash: string;
+  if (isDir) {
+    const files = glob.sync(`${path}/**`, {nodir: true});
+    const fileHashes = files.map(sha256sum).join(",");
+    hexHash = createHash('sha256').update(fileHashes).digest('hex');
+  } else {
+    hexHash = sha256sum(path);
+  }
+
   switch (format) {
     case 'hex':
       return hexHash;
