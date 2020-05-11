fe8386a6ed6d6324aa596a6f0b4fa487bb04c3bf
Kadeem Hassam <kadeem@unbounce.com>
2020-05-11 11:37:13 -0700

    Fixes spelling of `command-implementations`

--- src/tests/test-cli.ts
+++ src/tests/test-cli.ts
@@ -1,7 +1,7 @@
 import {expect} from 'chai';
 
 import {buildArgs, lazyLoad} from "../main";
-import {implementations} from "../cli/command-implemntations";
+import {implementations} from "../cli/command-implementations";
 import {} from "../cfn/approval/cli";
 import {} from "../cfn/approval/index";
 
@@ -21,7 +21,7 @@ describe('cli', () => {
   it('lazy loaded implementations are loadable', () => {
     expect(typeof lazyLoad('createStackMain')).to.equal('function');
     expect(implementations.createStackMain).to.equal(
-      require('../cli/command-implemntations').implementations.createStackMain);
+      require('../cli/command-implementations').implementations.createStackMain);
   });
 
 });
